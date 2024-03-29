7178cbb88cfb37f6ab697237a6e93fcfd71b70e7
TamNhien <canhthien91@gmail.com>
2024-03-29 13:40:48 +0700

    Add project files.

---
+++ P_TimKiTuHoaLonNhat.py
@@ -0,0 +1,4 @@
+﻿L = input("Nhập vào danh sách các chuỗi, cách nhau bởi dấu phẩy: ").split(',')
+max_upper_string = max(L, key=lambda s: max((i for i, c in enumerate(s) if c.isupper()), default=-1), default="")
+print(f"Chuỗi có vị trí ký tự in hoa lớn nhất là: {max_upper_string}" if max_upper_string else "Danh sách không có chuỗi nào có ký tự in hoa.")
+
